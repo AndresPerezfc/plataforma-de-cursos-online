17181a408356c8e161285684c9d87006478dc04a
andresperezfc <felipe3apc@gmail.com>
2021-04-10 13:26:10 -0500

    Creación de modelos y migraciones

--- app/Models/User.php
+++ app/Models/User.php
@@ -58,4 +58,11 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    // Relación uno a uno
+
+    public function profile()
+    {
+        return $this->hasOne('App\Models\Profile');
+    }
 }
